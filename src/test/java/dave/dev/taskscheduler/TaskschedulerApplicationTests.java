3db81138efdcf1cc0de99fd6707dac4d796326ca
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-01 17:30:07 +0800

    Task Scheduler

---
+++ src/test/java/dave/dev/taskscheduler/TaskschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.taskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
