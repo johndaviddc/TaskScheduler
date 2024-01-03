a92fdd7126d32b7f3a35b4c12f5d8d10fbe12c99
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-03 16:48:49 +0800

    added service package

---
+++ src/main/java/dave/dev/taskscheduler/service/TaskService.java
@@ -0,0 +1,12 @@
+package dave.dev.taskscheduler.service;
+
+import dave.dev.taskscheduler.model.Task;
+
+import java.util.List;
+
+public interface TaskService {
+    Task schedulerTask(Task task);
+    List<Task> getAllTasks();
+    Task getTaskById(Long id);
+    void deleteTask(Long id);
+}
